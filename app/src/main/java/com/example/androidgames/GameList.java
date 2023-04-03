99a3232c98f8893232c55f1fe4d0159e4a548b3c
Sene <albin.sene@gmail.com>
2023-04-03 10:42:24 +0200

    game list

--- app/src/main/java/com/example/androidgames/GameList.java
+++ app/src/main/java/com/example/androidgames/GameList.java
@@ -1,22 +1,14 @@
 package com.example.androidgames;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GameMode extends AppCompatActivity {
+public class GameList extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamemode);
     }
-
-    public void startGameList(View v){
-        Intent intent = new Intent(this, Questionnaire.class);
-        startActivity(intent);
-    }
-
-
 }

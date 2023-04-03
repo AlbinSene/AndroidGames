3a38879374d2718eec26c47e9fc3e6dd269f804c
Sene <albin.sene@gmail.com>
2023-04-03 09:58:01 +0200

    gamemode solo

---
+++ app/src/main/java/com/example/androidgames/GameMode.java
@@ -0,0 +1,13 @@
+package com.example.androidgames;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameMode extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}

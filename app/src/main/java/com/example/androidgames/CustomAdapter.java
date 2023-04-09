d33257a36139efb1a9a0306f34447773acd0ce5c
Sene <albin.sene@gmail.com>
2023-04-09 16:33:34 +0200

    list jeux

---
+++ app/src/main/java/com/example/androidgames/CustomAdapter.java
@@ -0,0 +1,88 @@
+package com.example.androidgames;
+
+//import static android.os.Build.VERSION_CODES.R;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class CustomAdapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<String>();
+    private Context context;
+
+
+
+    public CustomAdapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.row_list_games, null);
+        }
+
+        //Handle TextView and display string from your list
+        TextView listItemText = (TextView)view.findViewById(R.id.gamename);
+        listItemText.setText(list.get(position));
+
+        //Handle buttons and add onClickListeners
+        Button playBtn = (Button)view.findViewById(R.id.play_btn);
+
+
+        playBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                String s = list.get(position);
+
+                switch (s){
+                    case "light_sensor":
+                        Intent lightSensorIntent = new Intent(context, Light_sensor.class);
+                        context.startActivity(lightSensorIntent);
+                        break;
+                    case "mega_ball":
+                        Intent megaBallIntent = new Intent(context, MegaBall.class);
+                        context.startActivity(megaBallIntent);
+                        break;
+                    case "questionnaire":
+                        Intent questionnaireIntent = new Intent(context, Questionnaire.class);
+                        context.startActivity(questionnaireIntent);
+                        break;
+                }
+
+
+
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/androidgames/GameMode.java
+++ app/src/main/java/com/example/androidgames/GameMode.java
@@ -1,21 +1,39 @@
 package com.example.androidgames;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GameMode extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamemode);
+
+
     }
 
     public void startGameList(View v){
-        Intent intent = new Intent(this, Light_sensor.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, Light_sensor.class);
+        //startActivity(intent);
+
+        setContentView(R.layout.game_list);
+        String[] list = {"light_sensor","mega_ball","questionnaire"};
+        ArrayList listGames = new ArrayList<String>(Arrays.asList(list));
+
+        //instantiate custom adapter
+        CustomAdapter adapter = new CustomAdapter(listGames, this);
+
+        //handle listview and assign adapter
+        ListView lView = (ListView)findViewById(R.id.listGames);
+        lView.setAdapter(adapter);
     }
 
 
